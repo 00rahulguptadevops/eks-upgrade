9740752ad0fdde41444105bf5656cecf34abb6a4
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-21 13:02:45 +0530

    added code

--- src/org/foo/Human.groovy
+++ src/org/foo/Human.groovy
@@ -1,9 +1,16 @@
 package org.foo
 
-class Human {
-    String name
+class Human implements Serializable {
     def steps
+    String name
+
+    Human(steps, name) {
+        this.steps = steps
+        this.name = name
+    }
+
     void sayName() {
         steps.sh "echo Hello, my name is ${name}"
     }
 }
+
