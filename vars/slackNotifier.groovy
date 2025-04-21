96779f5d6de6076a1baa4f71f7159039275c207c
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-21 16:23:48 +0530

    added slack code

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,15 +1,47 @@
-def notifyStart(String channel, String tokenCredentialId) {
-    slackSend channel: channel, tokenCredentialId: tokenCredentialId, color: '#439FE0',
-              message: ":rocket: Job *${env.JOB_NAME}* started (Build #${env.BUILD_NUMBER})"
+def sendMessage(String status, String color, String webhookCredId) {
+    def message = ""
+    switch (status) {
+        case "start":
+            message = ":rocket: Job *${env.JOB_NAME}* started (Build #${env.BUILD_NUMBER})"
+            break
+        case "success":
+            message = ":white_check_mark: Job *${env.JOB_NAME}* completed successfully (Build #${env.BUILD_NUMBER})"
+            break
+        case "failure":
+            message = ":x: Job *${env.JOB_NAME}* failed (Build #${env.BUILD_NUMBER})"
+            break
+        default:
+            message = ":grey_question: Job status unknown"
+    }
+
+    withCredentials([string(credentialsId: webhookCredId, variable: 'SLACK_WEBHOOK')]) {
+        def payload = """
+        {
+            "attachments": [
+                {
+                    "color": "${color}",
+                    "text": "${message}"
+                }
+            ]
+        }
+        """
+
+        httpRequest httpMode: 'POST',
+                    contentType: 'APPLICATION_JSON',
+                    requestBody: payload,
+                    url: SLACK_WEBHOOK
+    }
 }
 
-def notifySuccess(String channel, String tokenCredentialId) {
-    slackSend channel: channel, tokenCredentialId: tokenCredentialId, color: 'good',
-              message: ":white_check_mark: Job *${env.JOB_NAME}* completed successfully (Build #${env.BUILD_NUMBER})"
+def notifyStart(String webhookCredId) {
+    sendMessage("start", "#439FE0", webhookCredId)
 }
 
-def notifyFailure(String channel, String tokenCredentialId) {
-    slackSend channel: channel, tokenCredentialId: tokenCredentialId, color: 'danger',
-              message: ":x: Job *${env.JOB_NAME}* failed (Build #${env.BUILD_NUMBER})"
+def notifySuccess(String webhookCredId) {
+    sendMessage("success", "good", webhookCredId)
+}
+
+def notifyFailure(String webhookCredId) {
+    sendMessage("failure", "danger", webhookCredId)
 }
 
