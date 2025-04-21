4cf6dd7e05ec33a5667d34ac16ca34b2e185cc45
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-21 16:57:23 +0530

    added deprication api code

---
+++ vars/checkDeprecatedApis.groovy
@@ -0,0 +1,28 @@
+def call(String dockerImage, String k8sTargetVersion) {
+    withFileParameter('KUBECONFIG') {
+        def output = sh(
+            script: """
+                /usr/local/bin/docker run -i --rm \
+                -v "$KUBECONFIG:/root/.kube/config" \
+                -v ~/.aws:/root/.aws \
+                ${dockerImage} \
+                -t ${k8sTargetVersion} -o json -e -k /root/.kube/config 2>/dev/null
+            """,
+            returnStdout: true
+        ).trim()
+
+        if (output) {
+            def jsonOutput = readJSON text: output
+            if (jsonOutput) {
+                echo "❌ Deprecated APIs found:"
+                jsonOutput.each { item ->
+                    echo "- ${item.kind} (${item.apiversion}) in namespace ${item.namespace}, replace with: ${item.replacement}"
+                }
+                error("Deprecated APIs detected.")
+            }
+        }
+
+        echo "✅ No deprecated APIs detected."
+    }
+}
+
