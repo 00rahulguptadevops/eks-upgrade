73102180d1557cc9562848450d98c4adec0a63ec
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-21 16:31:05 +0530

    added slack code

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,14 +1,14 @@
-def sendMessage(String status, String color, String webhookCredId) {
+def sendMessage(String status, String color, String webhookCredId, String stageName) {
     def message = ""
     switch (status) {
         case "start":
-            message = ":rocket: Job *${env.JOB_NAME}* started (Build #${env.BUILD_NUMBER})"
+            message = ":rocket: Job *${env.JOB_NAME}* started (Build #${env.BUILD_NUMBER}) in stage *${stageName}*"
             break
         case "success":
-            message = ":white_check_mark: Job *${env.JOB_NAME}* completed successfully (Build #${env.BUILD_NUMBER})"
+            message = ":white_check_mark: Job *${env.JOB_NAME}* completed successfully (Build #${env.BUILD_NUMBER}) at stage *${stageName}*"
             break
         case "failure":
-            message = ":x: Job *${env.JOB_NAME}* failed (Build #${env.BUILD_NUMBER})"
+            message = ":x: Job *${env.JOB_NAME}* failed (Build #${env.BUILD_NUMBER}) at stage *${stageName}*"
             break
         default:
             message = ":grey_question: Job status unknown"
@@ -33,15 +33,15 @@ def sendMessage(String status, String color, String webhookCredId) {
     }
 }
 
-def notifyStart(String webhookCredId) {
-    sendMessage("start", "#439FE0", webhookCredId)
+def notifyStart(String webhookCredId, String stageName = "N/A") {
+    sendMessage("start", "#439FE0", webhookCredId, stageName)
 }
 
-def notifySuccess(String webhookCredId) {
-    sendMessage("success", "good", webhookCredId)
+def notifySuccess(String webhookCredId, String stageName = "N/A") {
+    sendMessage("success", "good", webhookCredId, stageName)
 }
 
-def notifyFailure(String webhookCredId) {
-    sendMessage("failure", "danger", webhookCredId)
+def notifyFailure(String webhookCredId, String stageName = "N/A") {
+    sendMessage("failure", "danger", webhookCredId, stageName)
 }
 
