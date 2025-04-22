31757d2fb68efae4b3bbc2b21c7e5998b0bbec94
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-22 18:31:38 +0530

    updated check version code

--- vars/checkVersion.groovy
+++ vars/checkVersion.groovy
@@ -7,12 +7,14 @@ def call(String currentVersion, String targetVersion) {
     def current = parseVersion(currentVersion)
     def target = parseVersion(targetVersion)
 
-    if (target.major < current.major || 
-        (target.major == current.major && target.minor <= current.minor)) {
-        echo "✅ Allowed: Target version ${targetVersion} is <= current version ${currentVersion}"
+    if (target.major == current.major && target.minor == current.minor) {
+        echo "✅ Your cluster is already running with version ${targetVersion}"
+        return true
+    } else if (target.major == current.major && target.minor == current.minor + 1) {
+        echo "✅ Allowed: Upgrade to version ${targetVersion} is permitted from ${currentVersion}"
         return true
     } else {
-        error "❌ Denied: Target version ${targetVersion} is > current version ${currentVersion}"
+        error "❌ Denied: Target version ${targetVersion} is not allowed from current version ${currentVersion}"
     }
 }
 
