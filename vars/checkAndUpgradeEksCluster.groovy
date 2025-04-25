614a286ad0b2440d635fbee3857cba1b6049f704
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-25 11:31:35 +0530

    added cluster version check code

---
+++ vars/checkAndUpgradeEksCluster.groovy
@@ -0,0 +1,20 @@
+def call(String clusterName, String region, String targetVersion){
+    echo "Checking kubernetes version for eks cluster: ${clusterName} in region ${region}"
+
+    def currentVersion = sh(
+        script: """
+            /usr/local/bin/docker run --rm -v ~/.aws:/root/.aws public.ecr.aws/eksctl/eksctl get cluster --name ${clusterName} --region ${region} -o json | jq -r '.[0].Version'
+            """,
+            returnStdout: true
+    ).trim()
+    echo "Cluster CurrentVersion: ${currentVersion}"
+    echo "Taget Version: ${targetVersion}"
+
+    if (currentVersion == targetVersion) {
+        echo "Cluster already running with ${tagerVersion}"
+        return false //Indicate Skip
+    } else {
+        echo "Upgrading Cluster ${tagerVersion}"
+        return true
+    }
+}
