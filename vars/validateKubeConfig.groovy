35ba9c3064eb5f0c9d3db14980dcfdec9df2232d
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-30 14:36:18 +0530

    update shared lib code

--- vars/validateKubeConfig.groovy
+++ vars/validateKubeConfig.groovy
@@ -1,21 +1,23 @@
 def call(String kubeconfig) {
-    echo "Validating Kubernetes cluster access using kubeconfig: ${kubeconfig}"
+    echo "🔍 Validating Kubernetes cluster access using kubeconfig: ${kubeconfig}"
 
-    def result = sh(
+    def exitCode = sh(
         script: """
             /usr/local/bin/docker run --rm --network host \\
               -v ~/.aws:/root/.aws \\
               -v ${kubeconfig}:/file -e KUBECONFIG=/file \\
-              heyvaldemar/aws-kubectl sh -c '(kubectl cluster-info > /dev/null 2>&1 && echo "true") || (echo "false" && exit 1)'
+              heyvaldemar/aws-kubectl sh -c 'kubectl cluster-info > /dev/null 2>&1'
         """,
-        returnStdout: true
-    ).trim()
-     
-    echo result
+        returnStatus: true
+    )
 
-    def summary = result == "true"
-        ? ":white_check_mark: Kubeconfig file `${kubeconfig}` is valid\n"
-        : ":x: Kubeconfig file `${kubeconfig}` is invalid\n"
-
-    return [valid: result == "true", summary: summary]
+    if (exitCode != 0) {
+        def summary = ":x: Kubeconfig file `${kubeconfig}` is invalid"
+        echo summary
+        error("❌ Kubeconfig validation failed.\n${summary}")
+    } else {
+        def summary = ":white_check_mark: Kubeconfig file `${kubeconfig}` is valid"
+        echo summary
+        return [valid: true, summary: summary]
+    }
 }
