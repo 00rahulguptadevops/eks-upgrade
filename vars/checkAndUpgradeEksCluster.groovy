8db2d0cfdacb48c0d2243d71d0a0b079a21bf226
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-30 16:26:09 +0530

    update shared lib code

--- vars/checkAndUpgradeEksCluster.groovy
+++ vars/checkAndUpgradeEksCluster.groovy
@@ -1,47 +1,23 @@
-// def call(String clusterName, String region, String targetVersion){
-//     echo "Checking kubernetes version for eks cluster: ${clusterName} in region ${region}"
-
-//     def currentVersion = sh(
-//         script: """
-//             /usr/local/bin/docker run --rm -v ~/.aws:/root/.aws public.ecr.aws/eksctl/eksctl get cluster --name ${clusterName} --region ${region} -o json | jq -r '.[0].Version'
-//             """,
-//             returnStdout: true
-//     ).trim()
-//     echo "Cluster CurrentVersion: ${currentVersion}"
-//     echo "Taget Version: ${targetVersion}"
-   // "cluster ${clusterName} control plane has been upgraded to version ${targetVersion}"
-//     if (currentVersion == targetVersion) {
-//         echo "Cluster already running with ${targetVersion}"
-//         return false //Indicate Skip
-//     } else {
-//       //  /usr/local/bin/docker run --rm -v ~/.aws:/root/.aws public.ecr.aws/eksctl/eksctl upgrade cluster --name=${clusterName} --version=${targetVersion}  --region=${region} --approve
-//        echo "Updating"
-//         return true
-//     }
-// }
-
-
 def call(String clusterName, String region, String targetVersion) {
-    echo "Upgrading cluster to version: ${targetVersion}"
-    
+    echo "Upgrading cluster '${clusterName}' to version: ${targetVersion} in region: ${region}"
+
     def upgradeResult = sh(
-        script: "/usr/local/bin/docker run --rm -v ~/.aws:/root/.aws public.ecr.aws/eksctl/eksctl upgrade cluster --name=${clusterName} --version=${targetVersion}  --region=${region} --approve",
-       // script: "/usr/local/bin/docker run --rm -v /Users/rahulgupta/.aws:/root/.aws public.ecr.aws/eksctl/eksctl upgrade cluster --name=eks-cluster --version=1.31 --region=ap-south-1 --approve",
+        script: "/usr/local/bin/docker run --rm -v ~/.aws:/root/.aws public.ecr.aws/eksctl/eksctl upgrade cluster --name=${clusterName} --version=${targetVersion} --region=${region} --approve",
         returnStdout: true
     ).trim()
 
-    echo "Upgrade Result: ${upgradeResult}"
+    echo "Upgrade Result:\n${upgradeResult}"
 
     if (upgradeResult.contains("control plane has been upgraded to version")) {
-        return "true"
+        return [
+            summary: "✅ Cluster '${clusterName}' successfully upgraded to version ${targetVersion} in region ${region}.",
+            status: "success"
+        ]
     } else {
-        echo "Cluster upgrade failed"
-        return "false"
+        echo "Cluster upgrade failed for '${clusterName}'"
+        return [
+            summary: "❌ Upgrade failed for cluster '${clusterName}' to version ${targetVersion} in region ${region}.",
+            status: "failure"
+        ]
     }
 }
-
-
-
-
-
-
