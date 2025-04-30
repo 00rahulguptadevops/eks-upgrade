f8dc27b58651e9a3f4a0e0074f03b82b1c74e00d
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-30 12:08:31 +0530

    update shared lib code

--- vars/k8sApiVersionCheck.groovy
+++ vars/k8sApiVersionCheck.groovy
@@ -8,17 +8,9 @@ def call(String kubeconfig, String targetVersion) {
         """,
         returnStdout true
     ).trim()
+    return result
 }
 
 
 
 
-
-
-
-                                sh """
-                                    /usr/local/bin/docker run --rm --network host \\
-                                      -v ${kubePath}:/root/.kube/config \\
-                                      -v ~/.aws:/root/.aws \\
-                                      kubent:aws01 -t ${clusterInfo.target_version} -o json -e -k /root/.kube/config
-                                """
