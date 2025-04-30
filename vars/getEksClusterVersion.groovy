093b2f090236fe736c0ab31c87c95f6fd6795460
rahulgupta <rahulgupta@rahulguptas-MacBook-Pro.local>
2025-04-30 15:41:52 +0530

    update shared lib code

--- vars/getEksClusterVersion.groovy
+++ vars/getEksClusterVersion.groovy
@@ -1,4 +1,4 @@
-def call(String clusterName, String region) {
+def call(String clusterName, String region, String targetVersion) {
     def currentVersion = sh(
         script: "/usr/local/bin/docker run --rm -v ~/.aws:/root/.aws public.ecr.aws/eksctl/eksctl get cluster --name ${clusterName} --region ${region} -o json | jq -r '.[0].Version'",
         returnStdout: true
@@ -7,8 +7,8 @@ def call(String clusterName, String region) {
     echo "Current version of the cluster: ${currentVersion}"
     
 
-    if (currentVersion == clusterInfo.target_version) {
-        echo "Cluster is already at the target version: ${clusterInfo.target_version}. Skipping upgrade."
+    if (currentVersion == targetVersion) {
+        echo "Cluster is already at the target version: ${targetVersion}. Skipping upgrade."
         currentBuild.result = 'SUCCESS'
         return  // exit stage early
     }  
